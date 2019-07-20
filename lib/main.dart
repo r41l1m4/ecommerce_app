69266da9596220c57ea30179cfa39ed382aa4c49
Raí <rai.lima17@live.com>
2019-07-20 13:07:43 -0300

    add home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Loja Virtual",
+      title: "Flutters's Clothing",
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141),
       ),
-      home: Container(),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:ecommerce_app/tabs/home_tab.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        HomeTab(),
+      ],
+    );
+  }
+}
