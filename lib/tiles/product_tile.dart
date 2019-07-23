e64c1332579381c846e9e66b8910a35312a21edd
Raí <rai.lima17@live.com>
2019-07-22 23:19:59 -0300

    add products list view

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -2,7 +2,6 @@ import 'package:ecommerce_app/datas/product_data.dart';
 import 'package:flutter/material.dart';
 
 class ProductTile extends StatelessWidget {
-
   final String type;
   final ProductData product;
 
@@ -14,43 +13,80 @@ class ProductTile extends StatelessWidget {
       onTap: () {},
       child: Card(
         elevation: 5.0,
-        child: type == "grid" ?
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
+        child: type == "grid"
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
                     aspectRatio: 0.8,
-                  child: Image.network(
-                    product.images[0],
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text(product.title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Text(
-                          "R\$ ${product.price.toStringAsFixed(2)}",
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
+                    child: Image.network(
+                      product.images[0],
+                      fit: BoxFit.cover,
                     ),
                   ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            product.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Text(
+                            "R\$ ${product.price.toStringAsFixed(2)}",
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : Row(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Image.network(
+                product.images[0],
+                fit: BoxFit.cover,
+                height: 250.0,
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      product.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      "R\$ ${product.price.toStringAsFixed(2)}",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            )
-            : Row(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
