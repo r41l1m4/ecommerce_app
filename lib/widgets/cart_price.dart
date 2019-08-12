04b8ce5ae8473a626b904d9c0e4eb1b317b9e35e
Raí <rai.lima17@live.com>
2019-08-12 10:32:51 -0300

    add order resume logic

--- lib/widgets/cart_price.dart
+++ lib/widgets/cart_price.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartPrice extends StatelessWidget {
-
   final VoidCallback buy;
 
   CartPrice(this.buy);
@@ -16,6 +15,10 @@ class CartPrice extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
         child: ScopedModelDescendant<CartModel>(
           builder: (context, child, model) {
+            double price = model.getProductsPrice();
+            double discount = model.getDiscount();
+            double ship = model.getShipPrice();
+
             return Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
@@ -32,36 +35,26 @@ class CartPrice extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Text(
-                      "Subtotal"
-                    ),
-                    Text(
-                      "R\$ 0.00"
-                    ),
+                    Text("Subtotal"),
+                    Text("R\$ ${price.toStringAsFixed(2)}"),
                   ],
                 ),
                 Divider(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Text(
-                        "Desconto"
-                    ),
-                    Text(
-                        "R\$ 0.00"
-                    ),
+                    Text("Desconto"),
+                    Text(discount > 0
+                        ? "R\$ -${discount.toStringAsFixed(2)}"
+                        : "R\$ ${discount.toStringAsFixed(2)}"),
                   ],
                 ),
                 Divider(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Text(
-                        "Entrega"
-                    ),
-                    Text(
-                        "R\$ 0.00"
-                    ),
+                    Text("Entrega"),
+                    Text("R\$ ${ship.toStringAsFixed(2)}"),
                   ],
                 ),
                 Divider(),
@@ -72,13 +65,13 @@ class CartPrice extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     Text(
-                        "Total",
+                      "Total",
                       style: TextStyle(
                         fontWeight: FontWeight.w500,
                       ),
                     ),
                     Text(
-                        "R\$ 0.00",
+                      "R\$ ${(price - discount + ship).toStringAsFixed(2)}",
                       style: TextStyle(
                         color: Theme.of(context).primaryColor,
                         fontSize: 16.0,
