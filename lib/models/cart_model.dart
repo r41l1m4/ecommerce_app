807871669956a0c62570e6734db65812691acde0
Raí <rai.lima17@live.com>
2019-08-12 09:31:12 -0300

    add cart tile and retrieve cart data from firebase

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -11,9 +11,14 @@ class CartModel extends Model {
 
   bool isLoading = false;
 
-  CartModel(this.user);
+  CartModel(this.user) {
+    if(user.isLoggedIn()) {
+      _loadCartItems();
+    }
+  }
 
-  static CartModel of(BuildContext context) => ScopedModel.of<CartModel>(context);
+  static CartModel of(BuildContext context) =>
+      ScopedModel.of<CartModel>(context);
 
   void addCartItem(CartProduct product) {
     products.add(product);
@@ -39,6 +44,44 @@ class CartModel extends Model {
         .delete();
 
     products.remove(product);
+
+    notifyListeners();
+  }
+
+  void decProduct(CartProduct cartProduct) {
+    cartProduct.quantity--;
+
+    Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .document(cartProduct.cid)
+        .updateData(cartProduct.toMap());
+
+    notifyListeners();
+  }
+
+  void incProduct(CartProduct cartProduct) {
+    cartProduct.quantity++;
+
+    Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .document(cartProduct.cid)
+        .updateData(cartProduct.toMap());
+
+    notifyListeners();
+  }
+
+  Future _loadCartItems() async {
+    QuerySnapshot query = await Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .getDocuments();
+
+    products = query.documents.map((doc) => CartProduct.fromDocument(doc)).toList();
 
     notifyListeners();
   }
--- lib/tiles/cart_tile.dart
+++ lib/tiles/cart_tile.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_app/datas/cart_product.dart';
 import 'package:ecommerce_app/datas/product_data.dart';
+import 'package:ecommerce_app/models/cart_model.dart';
 import 'package:flutter/material.dart';
 
 class CartTile extends StatelessWidget {
@@ -9,15 +10,89 @@ class CartTile extends StatelessWidget {
 
   CartTile(this.cartProduct);
 
-  Widget _buildContent() {
-
-  }
-
   @override
   Widget build(BuildContext context) {
+
+    Widget _buildContent() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8.0),
+            width: 120.0,
+            child: Image.network(
+              cartProduct.productData.images[0],
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    cartProduct.productData.title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 17.0,
+                    ),
+                  ),
+                  Text(
+                    "Tamanho: ${cartProduct.size}",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Text(
+                    "R\$ ${cartProduct.productData.price.toStringAsFixed(2)}",
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.remove),
+                        color: Theme.of(context).primaryColor,
+                        onPressed: cartProduct.quantity > 1 ? () {
+                          CartModel.of(context).decProduct(cartProduct);
+                        } : null,
+                      ),
+                      Text(
+                        cartProduct.quantity.toString(),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        color: Theme.of(context).primaryColor,
+                        onPressed: () {
+                          CartModel.of(context).incProduct(cartProduct);
+                        },
+                      ),
+                      FlatButton(
+                        child: Text("Remover"),
+                        textColor: Colors.grey[500],
+                        onPressed: (){
+                          CartModel.of(context).removeCartItem(cartProduct);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-      child: cartProduct == null ?
+      child: cartProduct.productData == null ?
       FutureBuilder<DocumentSnapshot>(
         future: Firestore.instance.collection("products").document(
             cartProduct.category).collection("items")
